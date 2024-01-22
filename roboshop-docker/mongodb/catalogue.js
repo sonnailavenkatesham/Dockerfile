c833747d8cef5e7cd193d02659d0c3a794fbb412
Roman Reings <Rajkumar>
2024-01-22 21:01:32 +0530

    added Dockerfiles

--- roboshop-docker/mongodb/catalogue.js
+++ roboshop-docker/mongodb/catalogue.js
@@ -1,193 +1,30 @@
-const mongoClient = require('mongodb').MongoClient;
-const mongoObjectID = require('mongodb').ObjectID;
-const bodyParser = require('body-parser');
-const express = require('express');
-const pino = require('pino');
-const expPino = require('express-pino-logger');
+//
+// Products
+//
+db = db.getSiblingDB('catalogue');
+db.products.insertMany([
+    {sku: 'HAL-1', name: 'HAL', description: 'Sorry Dave, I cant do that', price: 2001, instock: 2, categories: ['Artificial Intelligence']},
+    {sku: 'PB-1', name: 'Positronic Brain', description: 'Highly advanced sentient processing unit with the laws of robotics burned in', price: 200, instock: 0, categories: ['Artificial Intelligence']},
+    {sku: 'ROB-1', name: 'Robbie', description: 'Large mechanical workhorse, crude but effective. Comes in handy when you are lost in space', price: 1200, instock: 12, categories: ['Robot']},
+    {sku: 'EVE-1', name: 'Eve', description: 'Extraterrestrial Vegetation Evaluator', price: 5000, instock: 10, categories: ['Robot']},
+    {sku: 'C3P0', name: 'C3P0', description: 'Protocol android', price: 953, instock: 1, categories: ['Robot']},
+    {sku: 'R2D2', name: 'R2D2', description: 'R2 maintenance robot and secret messenger. Help me Obi Wan', price: 1024, instock: 1, categories: ['Robot']},
+    {sku: 'K9', name: 'K9', description: 'Time travelling companion at heel', price: 300, instock: 12, categories: ['Robot']},
+    {sku: 'RD-10', name: 'Kryten', description: 'Red Drawf crew member', price: 700, instock: 5, categories: ['Robot']},
+    {sku: 'HHGTTG', name: 'Marvin', description: 'Marvin, your paranoid android. Brain the size of a planet', price: 42, instock: 48, categories: ['Robot']},
+    {sku: 'STAN-1', name: 'Stan', description: 'APM guru', price: 67, instock: 1000, categories: ['Robot', 'Artificial Intelligence']},
+    {sku: 'STNG', name: 'Mr Data', description: 'Could be R. Daneel Olivaw? Protype positronic brain android', price: 1000, instock: 0, categories: ['Robot']}
+]);
 
-const logger = pino({
-    level: 'info',
-    prettyPrint: false,
-    useLevelLabels: true
-});
-const expLogger = expPino({
-    logger: logger
-});
-
-// MongoDB
-var db;
-var collection;
-var mongoConnected = false;
-
-const app = express();
-
-app.use(expLogger);
-
-app.use((req, res, next) => {
-    res.set('Timing-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Origin', '*');
-    next();
+// full text index for searching
+db.products.createIndex({
+    name: "text",
+    description: "text"
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// unique index for product sku
+db.products.createIndex(
+    { sku: 1 },
+    { unique: true }
+);
 
-app.get('/health', (req, res) => {
-    var stat = {
-        app: 'OK',
-        mongo: mongoConnected
-    };
-    res.json(stat);
-});
-
-// all products
-app.get('/products', (req, res) => {
-    if(mongoConnected) {
-        collection.find({}).toArray().then((products) => {
-            res.json(products);
-        }).catch((e) => {
-            req.log.error('ERROR', e);
-            res.status(500).send(e);
-        });
-    } else {
-        req.log.error('database not available');
-        res.status(500).send('database not avaiable');
-    }
-});
-
-// product by SKU
-app.get('/product/:sku', (req, res) => {
-    if(mongoConnected) {
-        // optionally slow this down
-        const delay = process.env.GO_SLOW || 0;
-        setTimeout(() => {
-        collection.findOne({sku: req.params.sku}).then((product) => {
-            req.log.info('product', product);
-            if(product) {
-                res.json(product);
-            } else {
-                res.status(404).send('SKU not found');
-            }
-        }).catch((e) => {
-            req.log.error('ERROR', e);
-            res.status(500).send(e);
-        });
-        }, delay);
-    } else {
-        req.log.error('database not available');
-        res.status(500).send('database not available');
-    }
-});
-
-// products in a category
-app.get('/products/:cat', (req, res) => {
-    if(mongoConnected) {
-        collection.find({ categories: req.params.cat }).sort({ name: 1 }).toArray().then((products) => {
-            if(products) {
-                res.json(products);
-            } else {
-                res.status(404).send('No products for ' + req.params.cat);
-            }
-        }).catch((e) => {
-            req.log.error('ERROR', e);
-            res.status(500).send(e);
-        });
-    } else {
-        req.log.error('database not available');
-        res.status(500).send('database not avaiable');
-    }
-});
-
-// all categories
-app.get('/categories', (req, res) => {
-    if(mongoConnected) {
-        collection.distinct('categories').then((categories) => {
-            res.json(categories);
-        }).catch((e) => {
-            req.log.error('ERROR', e);
-            res.status(500).send(e);
-        });
-    } else {
-        req.log.error('database not available');
-        res.status(500).send('database not available');
-    }
-});
-
-// search name and description
-app.get('/search/:text', (req, res) => {
-    if(mongoConnected) {
-        collection.find({ '$text': { '$search': req.params.text }}).toArray().then((hits) => {
-            res.json(hits);
-        }).catch((e) => {
-            req.log.error('ERROR', e);
-            res.status(500).send(e);
-        });
-    } else {
-        req.log.error('database not available');
-        res.status(500).send('database not available');
-    }
-});
-
-if (process.env.MONGO == 'true') {
-// set up Mongo
-function mongoConnect() {
-    return new Promise((resolve, reject) => {
-        var mongoURL = process.env.MONGO_URL || 'mongodb://mongodb:27017/catalogue';
-        mongoClient.connect(mongoURL, (error, client) => {
-            if(error) {
-                reject(error);
-            } else {
-                db = client.db('catalogue');
-                collection = db.collection('products');
-                resolve('connected');
-            }
-        });
-    });
-}
-}
-
-if (process.env.DOCUMENTDB == 'true') {
-function mongoConnect() {
-    return new Promise((resolve, reject) => {
-    var mongoURL = process.env.MONGO_URL || 'mongodb://username:password@mongodb:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
-    var client = mongoClient.connect(mongoURL,
-      {
-        // Mutable & Immutable
-        //tlsCAFile: `/home/roboshop/catalogue/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
-        // Container
-        tlsCAFile: `/app/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
-    }, (error, client) => {
-    if(error) {
-        reject(error);
-    } else {
-        db = client.db('catalogue');
-        collection = db.collection('products');
-        resolve('connected');
-    }
-});
-});
-}
-}
-
-
-// mongodb connection retry loop
-function mongoLoop() {
-    mongoConnect().then((r) => {
-        mongoConnected = true;
-        logger.info('MongoDB connected');
-    }).catch((e) => {
-        logger.error('ERROR', e);
-        setTimeout(mongoLoop, 2000);
-    });
-}
-
-mongoLoop();
-
-// fire it up!
-const port = process.env.CATALOGUE_SERVER_PORT || '8080';
-const MONGO = process.env.MONGO || 'false';
-const DOCUMENTDB = process.env.DOCUMENTDB || 'false';
-
-app.listen(port, () => {
-    logger.info('Started on port', port);
-});
