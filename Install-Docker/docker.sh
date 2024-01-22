842af13840c690b819100ab5d4c9ef47714f2653
Roman Reings <Rajkumar>
2024-01-22 22:09:06 +0530

    added Dockerfiles

--- Install-Docker/docker.sh
+++ Install-Docker/docker.sh
@@ -9,3 +9,7 @@ sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin dock
 sudo systemctl start docker
 
 sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
